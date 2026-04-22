d70cd4430d7896d97b9636d98eb826d536b3932a
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:22 -0500

    TUnit conversion

--- test/Soenneker.Blazor.Utils.Cookies.Tests/CookiesUtilTests.cs
+++ test/Soenneker.Blazor.Utils.Cookies.Tests/CookiesUtilTests.cs
@@ -1,20 +1,19 @@
 using Soenneker.Blazor.Utils.Cookies.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Blazor.Utils.Cookies.Tests;
 
-[Collection("Collection")]
-public sealed class CookiesUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class CookiesUtilTests : HostedUnitTest
 {
     private readonly ICookiesUtil _blazorlibrary;
 
-    public CookiesUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public CookiesUtilTests(Host host) : base(host)
     {
         _blazorlibrary = Resolve<ICookiesUtil>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
